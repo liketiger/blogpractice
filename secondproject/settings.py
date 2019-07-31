d3317b77b46d10101bbf12e1d170f81463612f7a
SungJae <edx2fire@likelion.org>
2019-07-31 12:35:03 +0900

    last

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['django-env.xu4gwnkbpf.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['django-env.xu4gwnkbpf.us-west-2.elasticbeanstalk.com', 'https://quiet-fjord-78024.herokuapp.com/']
 
 
 # Application definition
